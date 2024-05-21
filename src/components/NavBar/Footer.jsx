d81fe80e011777d58c06c262c6019152d1c71181
2211cs010458 <varunharinath15@gmail.com>
2024-05-21 18:37:43 +0530

    Update on Logo

--- src/components/NavBar/Footer.jsx
+++ src/components/NavBar/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { footerNavs } from "../../constants";
 import Logo from "../../assets/tesract-main.png";
-import { Twitter, Facebook, Globe, Youtube } from "lucide-react";
+import { Twitter, Facebook, LinkedinIcon } from "lucide-react";
 import ScrollToTopButton from "./utils/ScrollToTopButton";
 
 const Footer = () => {
@@ -10,13 +10,13 @@ const Footer = () => {
     <>
       <footer className="text-gray-100 px-4 py-5 max-w-screen-xl mx-auto md:px-8 mt-24">
         <div className="max-w-3xl sm:mx-auto sm:text-center">
-          <img src={Logo} className="w-32 sm:mx-auto" />
-          <p className="leading-relaxed mt-4 text-base font-light">
-            Tesseract is dedicated to empowering student-driven communities and
-            college-based clubs, particularly in the fields of technology, AI,
-            ML, and development. We strive to provide resources, support, and a
-            platform for students to collaborate, innovate, and achieve their
-            full potential.
+          <img src={Logo} className="w-36 h-auto  " />
+          <p className="leading-relaxed  text-base font-light">
+            Tesseract empowers student-driven communities and college clubs in
+            tech, AI, ML, and development. Our platform offers seamless payment
+            gateways and efficient event management, ensuring smooth
+            transactions and organized events for participants and event
+            managers.
           </p>
         </div>
         <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
@@ -44,16 +44,9 @@ const Footer = () => {
                   <Facebook className="w-6 h-6 text-blue-700" />
                 </a>
               </li>
-
               <li className="w-10 h-10 flex items-center justify-center">
                 <a href="javascript:void()">
-                  <Globe className="w-6 h-6 text-blue-500" />
-                </a>
-              </li>
-
-              <li className="w-10 h-10 flex items-center justify-center">
-                <a href="javascript:void()">
-                  <Youtube className="w-6 h-6 text-red-600" />
+                  <LinkedinIcon className="w-6 h-6 text-blue-700" />
                 </a>
               </li>
             </ul>
