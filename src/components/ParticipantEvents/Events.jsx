970d3bc9ff0f0382ace661039dafdbd256f28ce5
2211cs010458 <varunharinath15@gmail.com>
2024-05-23 20:33:49 +0530

    Update: on evets

--- src/components/ParticipantEvents/Events.jsx
+++ src/components/ParticipantEvents/Events.jsx
@@ -23,50 +23,99 @@ const Events = () => {
     <>
       {eventData.map((event) => {
         const { year, formattedDate } = formatDate(event.eventDate);
-        return (
-          <>
-            {/* On Going Events */}
-            {/* past events */}
-
-            <div></div>
-
-            <article className=" mx-5 flex my-6  transition hover:shadow-2xl  dark:shadow-gray-800/25 sm:mx-7 md:mx-20 lg:mx-28 border border-gray-800 rounded-3xl p-3">
-              <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
-                <time
-                  datetime="{event.eventDate}"
-                  className="flex items-center justify-between gap-4 text-xs font-semibold uppercase text-gray-900 dark:text-gray-200"
-                >
-                  <span>{year}</span>
-                  <span className="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
-                  <span>{formattedDate}</span>
-                </time>
-              </div>
-
-              <div className="flex flex-1 flex-col justify-between">
-                <div className="border-s border-gray-900/10 p-4 sm:!border-l-transparent sm:p-6 dark:border-white/10">
-                  <p>
-                    <h3 className="font-bold uppercase text-gray-900 dark:text-white">
-                      {event.eventName}
-                    </h3>
-                  </p>
-
-                  <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700 dark:text-gray-300">
-                    {event.eventDescription}
-                  </p>
-                </div>
-
-                <div className="sm:flex sm:items-end sm:justify-end">
-                  <a
-                    href="#"
-                    className="block bg-indigo-600 px-5 py-3 text-center text-xs font-bold uppercase text-gray-100 transition hover:bg-indigo-700 rounded-3xl"
+        if (event.eventStaus) {
+          return (
+            <>
+              <article className=" mx-5 flex my-6  transition hover:shadow-2xl  dark:shadow-gray-800/25 sm:mx-7 md:mx-20 lg:mx-28 border border-gray-800 rounded-3xl p-3">
+                <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
+                  <time
+                    datetime="{event.eventDate}"
+                    className="flex items-center justify-between gap-4 text-xs font-semibold uppercase text-gray-900 dark:text-gray-200"
                   >
-                    Book your event
-                  </a>
+                    <span>{year}</span>
+                    <span className="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                    <span>{formattedDate}</span>
+                  </time>
                 </div>
-              </div>
-            </article>
-          </>
-        );
+
+                <div className="flex flex-1 flex-col justify-between">
+                  <div className="border-s border-gray-900/10 p-4 sm:!border-l-transparent sm:p-6 dark:border-white/10">
+                    <p>
+                      <h3 className="font-bold uppercase text-gray-900 dark:text-white text-lg">
+                        {event.eventName}
+
+                        <span className=" mx-2 inline-flex items-center justify-center rounded-full bg-emerald-100 px-2.5 py-0.5 text-emerald-700 dark:bg-emerald-700 dark:text-emerald-100 ">
+                          <p className="whitespace-nowrap text-xs font-light">
+                            On Going
+                          </p>
+                        </span>
+                      </h3>
+                    </p>
+
+                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-600 dark:text-gray-300">
+                      {event.eventDescription}
+                    </p>
+                  </div>
+
+                  <div className="sm:flex sm:items-end sm:justify-end">
+                    <a
+                      href="#"
+                      className="block bg-indigo-600 px-5 py-3 text-center text-xs font-bold uppercase text-gray-100 transition hover:bg-indigo-700 rounded-3xl"
+                    >
+                      Book your event
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </>
+          );
+        }
+        if (!event.eventStaus) {
+          return (
+            <>
+              <article className=" mx-5 flex my-6  transition hover:shadow-2xl  dark:shadow-gray-800/25 sm:mx-7 md:mx-20 lg:mx-28 border border-gray-800 rounded-3xl p-3">
+                <div className="rotate-180 p-2 [writing-mode:_vertical-lr]">
+                  <time
+                    datetime="{event.eventDate}"
+                    className="flex items-center justify-between gap-4 text-xs font-semibold uppercase text-gray-900 dark:text-gray-200"
+                  >
+                    <span>{year}</span>
+                    <span className="w-px flex-1 bg-gray-900/10 dark:bg-white/10"></span>
+                    <span>{formattedDate}</span>
+                  </time>
+                </div>
+
+                <div className="flex flex-1 flex-col justify-between">
+                  <div className="border-s border-gray-900/10 p-4 sm:!border-l-transparent sm:p-6 dark:border-white/10">
+                    <p>
+                      <h3 className="font-bold uppercase text-gray-900 dark:text-white">
+                        {event.eventName}
+                        <span className=" mx-2 inline-flex items-center justify-center rounded-full bg-amber-100 px-2.5 py-0.5 text-amber-700 dark:bg-amber-700 dark:text-amber-100">
+                          <p className="whitespace-nowrap text-xs font-light">
+                            Completed
+                          </p>
+                        </span>
+                      </h3>
+                    </p>
+
+                    <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700 dark:text-gray-300">
+                      {event.eventDescription}
+                    </p>
+                  </div>
+
+                  <div className="sm:flex sm:items-end sm:justify-end">
+                    <a
+                      href="#"
+                      className="block bg-indigo-600 px-5 py-3 text-center text-xs font-bold uppercase text-gray-100 transition hover:bg-indigo-700 rounded-3xl disabled:opacity-50 disabled:cursor-not-allowed"
+                    >
+                      Book your event
+                    </a>
+                  </div>
+                </div>
+              </article>
+            </>
+          );
+        }
       })}
     </>
   );
